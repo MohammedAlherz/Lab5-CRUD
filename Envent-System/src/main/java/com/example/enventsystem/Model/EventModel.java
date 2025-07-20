41c46809f547429f646a6d4f69dafc39f0179210
MohammedAlherz <moh300moh43@gmail.com>
2025-07-20 14:25:16 +0300

    changing from LocalDate to LocalDateTime

--- Envent-System/src/main/java/com/example/enventsystem/Model/EventModel.java
+++ Envent-System/src/main/java/com/example/enventsystem/Model/EventModel.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -13,8 +13,9 @@ public class EventModel {
     private String description;
     private int capacity;
 
-    @JsonFormat(pattern="yyyy-MM-dd")
-    private LocalDate startDate;
-    @JsonFormat(pattern="yyyy-MM-dd")
-    private LocalDate endDate;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
+    private LocalDateTime startDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
+    private LocalDateTime endDate;
 }
